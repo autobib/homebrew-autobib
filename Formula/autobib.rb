352c46ee2aabb106bd1e0f0fbedec93b68ee551d
Peiran Wu <15968905+wupr@users.noreply.github.com>
2025-10-04 16:47:00 +0000

    autobib: update 0.4.1 bottle.

--- Formula/autobib.rb
+++ Formula/autobib.rb
@@ -8,11 +8,11 @@ class Autobib < Formula
   head "https://github.com/autobib/autobib.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/autobib/homebrew-autobib/releases/download/autobib-0.4.0_1"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b9c12c1251c948156f866b2f251a0b40d3bd1ed8ad9f944a81a78fc783d731b1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1d982465e48f86f0c3e7f26f3acbadfe8e1f45269e1f5c255f6d68a506ae3800"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "30bcad9730f6e9da112a61ce1c00107e14a064dbe99ae011fe3aef174daa367f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f4dc06ba7ed74825a8ce48c17f483bda459a2b9a55278842708a69dbc2eae0ae"
+    root_url "https://github.com/autobib/homebrew-autobib/releases/download/autobib-0.4.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ee0d381110efbc086e2d7e5edd494a86ded3bbfe56ace72b818285cb30405eb5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b29d552309bff16a87b945f51d832e2faadc6b055bd9e72c7f048281fe7b1577"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9dcd7028b8b01dc3e2811891bc5ae975226fd4b8b0b976388c123e5c5f6c5b9a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d791135017c9ccacac7718a9ae1146ede9126941dff142c9dbe82752ba8f908"
   end
 
   depends_on "cargo-about" => :build
