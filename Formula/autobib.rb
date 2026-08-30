717437a585324cc91d9dfb4175313b80919eb86e
Alex Rutar <alex@rutar.org>
2026-08-30 08:34:16 +0000

    autobib 0.7.0
    Closes #20.
    
    Signed-off-by: autobib-brew-bot <285231493+autobib-brew-bot@users.noreply.github.com>

--- Formula/autobib.rb
+++ Formula/autobib.rb
@@ -1,8 +1,8 @@
 class Autobib < Formula
   desc "Command-line tool for managing bibliographic records"
   homepage "https://github.com/autobib/autobib"
-  url "https://github.com/autobib/autobib/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "859432b9591c8d63cc047aaddc214c1c9f93047ba1ba327ac5ce22f62bc8b75e"
+  url "https://github.com/autobib/autobib/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "5f9c58be5046918563c6fbeb7f0211a329f50edb2b36c026b4dbfc05c3fd3f43"
   license "AGPL-3.0-or-later"
 
   head "https://github.com/autobib/autobib.git", branch: "main"
